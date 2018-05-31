3199955c30f3ab90a3261dc19ccd231130f94fd8
Space Mac <spacemac@Daniels-MacBook-Pro.local>
2018-05-31 12:40:40 -0700

    Added routes

--- routes/html_routes.js
+++ routes/html_routes.js
@@ -5,3 +5,27 @@ module.exports = function(app) {
     res.render("home");
   });
 };
+
+var router = require("express").Router();
+
+const stories = [
+  {
+    tagLine:
+      "Hotshot Commencement Speaker Jumps Straight Into Speech Without Even Defining ‘Courage’.",
+    description: 33
+  },
+  {
+    tagLine:
+      "Waste Of Time: Starbucks Closed Down For Anti-Bias Training Today Even Though We’ve Moved On And Are Mad About Other Things Now.",
+    description: 33
+  }
+];
+
+module.exports = function(app) {
+  app.get("/", function(req, res) {
+    res.render("home", {
+      title: "Home",
+      stories: stories
+    });
+  });
+};
