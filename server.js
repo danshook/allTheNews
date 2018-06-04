9170b0a48a8b7687c5134d090964077cff1edefb
Space Mac <spacemac@Daniels-MacBook-Pro.local>
2018-06-04 14:53:51 -0700

    added scraping

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ var express = require("express");
 var bodyParser = require("body-parser");
 // var logger = require("morgan");
 var mongoose = require("mongoose");
+var request = require("request");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
 
 // Require all models
 var db = require("./models");
@@ -27,6 +34,49 @@ require("./routes/html_routes")(app);
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/allTheNews");
 
+// Routes
+
+// A GET route for scraping the Glassdoor website
+app.get("/scrape", function(req, res) {
+  // First, we grab the body of the html with request
+  axios
+    .get("https://www.indeed.com/m/jobs?q=web+developer&l=Phoenix%2C+AZ")
+    .then(function(response) {
+      // Then, we load that into cheerio and save it to $ for a shorthand selector
+      var $ = cheerio.load(response.data);
+
+      // Now, we grab every h2 within an article tag, and do the following:
+      $("jobtitle").each(function(i, element) {
+        // Save an empty result object
+        var result = {};
+
+        // Add the text and href of every link, and save them as properties of the result object
+        result.title = $(this)
+          .children("a")
+          .text();
+        result.link = $(this)
+          .children("a")
+          .attr("href");
+
+        // Create a new Article using the `result` object built from scraping
+        db.Article.create(result)
+          .then(function(dbArticle) {
+            // View the added result in the console
+            console.log(dbArticle);
+          })
+          .catch(function(err) {
+            // If an error occurred, send it to the client
+            return res.json(err);
+          });
+        console.log(err);
+      });
+
+      // If we were able to successfully scrape and save an Article, send a message to the client
+      res.send("Scrape Complete");
+    });
+});
+
+// Start the server
 app.listen(PORT, function() {
   console.log("Listening on port:%s", PORT);
 });
