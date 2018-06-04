d4b06d57e45da1c34bcab45b1cc8cd8c78ad2f1f
Space Mac <spacemac@Daniels-MacBook-Pro.local>
2018-06-03 18:40:45 -0700

    more html

--- routes/html_routes.js
+++ routes/html_routes.js
@@ -1,10 +1,10 @@
 var router = require("express").Router();
 
-module.exports = function(app) {
-  app.get("/", function(req, res) {
-    res.render("home");
-  });
-};
+// module.exports = function(app) {
+//   app.get("/", function(req, res) {
+//     res.render("home");
+//   });
+// };
 
 var router = require("express").Router();
 
